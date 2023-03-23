b19bae0aa5e34ce75992ce85ab001b2285dd050f
EmdadHusain <100661175+EmdadHusain@users.noreply.github.com>
2023-03-23 10:21:03 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
 $(document).ready(function () {
-  $('.main-menu').meanmenu({
-    meanMenuContainer: '#mobile-menu',
-    meanScreenWidth:767,
+  // $('.main-menu').meanmenu({
+  //   meanMenuContainer: '#mobile-menu',
+  //   meanScreenWidth: 991,
+  // });
+  $(window).on('scroll',function(){
+    if($(this).scrollTop()>100){
+      $(".top-header").addClass("sticky");
+      $(".scroll_icn").css("display","block");
+    }else{
+      $(".top-header").removeClass("sticky");
+      $(".scroll_icn").css("display","none");
+    }
+  });
+  $(".scroll_icn").on('click',function(){
+    $('html,body').animate({
+      scrollTop:0
+    },500)
   });
   $('.business-carousel').owlCarousel({
     items: 1,
     loop: true,
     dots: true,
-    autoplay:true,
-    autoplayTimeout:2500,
-   
+    autoplay: true,
+    autoplayTimeout: 2500,
   });
-  $(".project-titles li").click(function(){
-    var selector= $(this).attr('data-filter');
-    $('.project-lists').isotope({
-        filter:selector
-    })
-})
-$('.video-play-btn').magnificPopup({
-  type:'video',
-})
-
+  $('.project-titles li').click(function () {
+    var selector = $(this).attr('data-filter');
+    $('.projects').isotope({
+      filter: selector,
+    });
+  });
+  $('.video-play-btn').magnificPopup({
+    type: 'video',
+  });
 });
-
